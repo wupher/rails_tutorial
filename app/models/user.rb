ab1345ebf9595fe269d643b7ece431dfa042f84a
Wupher Fan <wupher.fan@gmail.com>
2010-08-17 15:04:29 +0800

    添加了新字段，加密了密码，并提供了密码校验功能

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,61 @@
 # == Schema Information
-# Schema version: 20100816080846
+# Schema version: 20100816164003
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
 #
-
+require 'digest'
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  #password_confirmation will be created by validates_confirmation automaticly
+  attr_accessible :name, :email, :password_confirmation, :password 
+  attr_accessor :password
+  
+  before_save :encrypt_password
+  
   validates_presence_of :name
   validates_length_of :name, :within => 3..50
   
   EmailRegex = /\A[\w+\_.]+@[a-z\d\_.]+\.[a-z]+\z/i
   validates_format_of :email, :with => EmailRegex
   validates_uniqueness_of :email
+  
+  validates_confirmation_of :password
+  validates_presence_of :password
+  validates_length_of :password, :within => 6..20
+  
+  #return true if the user's password matches the submitted password.
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+  
+  def self.authenticate(email, password)
+    user = User.find_by_email(email)    
+    return nil if user.nil?
+    return user if user.has_password?(password)
+  end
+  
+  private 
+    def encrypt_password
+      self.salt = make_salt
+      self.encrypted_password = encrypt(password)
+    end
+    
+    def make_salt
+      secure_hash("#{Time.now.utc}#{password}")
+    end
+    
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
+    
+    def encrypt(string)
+      secure_hash("#{salt}#{string}")
+    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,10 @@ require 'spec_helper'
 describe User do
   before(:each) do
     @valid_attributes = {
-      :name => "value for name",
-      :email => "sample@tom.com"
+      :name => "Marshall Mather",
+      :email => "sample@tom.com",
+      :password => "Not Afraid",
+      :password_confirmation => 'Not Afraid'
     }
   end
 
@@ -33,8 +35,73 @@ describe User do
   
   it "should reject duplicate emails" do
     duplicate_email = "tom@tom.com"
-    User.create!(:name => 'tom', :email => duplicate_email)
+    User.create!(@valid_attributes.merge(:name => 'tom', :email => duplicate_email))
     duplicate_email_user = User.new(:name => 'Harry', :email => duplicate_email)
     duplicate_email_user.should_not be_valid
   end
+  
+  describe "password validation" do
+    
+    it "should  require a password" do
+      User.new(@valid_attributes.merge(:password => '', :password_confirmation => '')).should_not be_valid
+    end
+    
+    it "should  require matching password confirmation" do
+      User.new(@valid_attributes.merge(:password_confirmation => 'invalid')).should_not be_valid
+    end
+    
+    it "should reject short passwords" do
+      short_pwd = 'a' * 5;
+      User.new(@valid_attributes.merge(:password => short_pwd, :password_confirmation => short_pwd)).should_not be_valid
+    end
+    
+    it "should reject too long passwords" do
+      long_pwd = 'a' * 21;
+      User.new(@valid_attributes.merge(:password => long_pwd, :password_confirmation => long_pwd)).should_not be_valid
+    end
+  end #end of password validattion tests
+  
+  describe "password encryption" do
+    before(:each) do
+      @user = User.create!(@valid_attributes)
+    end
+    
+    it "should have an encrypted password attribute" do
+      @user.should respond_to(:encrypted_password)
+    end
+    
+    it 'should set the encrypted password' do
+      @user.encrypted_password.should_not be_blank
+    end
+    
+      describe 'has_password? method' do
+        it "should return true if the passwords match" do
+          @user.has_password?(@valid_attributes[:password]).should be_true
+        end
+        
+        it "should return false the passwords don't match" do
+          @user.has_password?('invalid').should be_false
+        end
+      
+      end# end of has_password? describe
+      
+      describe "authenticate method" do
+        
+        it 'should return nil on email/password missmatch' do
+          wrong_pwd_user = User.authenticate(@valid_attributes[:email], 'wrong pass')
+          wrong_pwd_user.should be_nil
+        end
+        
+        it 'should return nil on email/password on no user' do
+          wrong_email_user = User.authenticate('t@t.com', @valid_attributes[:password])
+          wrong_email_user.should be_nil
+        end
+        
+        it 'should return user on email/password match' do
+          match_user = User.authenticate(@valid_attributes[:email], @valid_attributes[:password])
+          match_user.should == @user
+        end
+      end
+  end #end of password encrypted
+  
 end
