09e4c512034d07fad9fd2f9d7cb71f9bcf7f4c31
Wupher Fan <wupher.fan@gmail.com>
2010-08-17 23:18:29 +0800

    User model with passwords, and user show page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,6 +4,12 @@ describe UsersController do
   integrate_views
   
   describe "GET 'new'" do
+    
+    before(:each) do
+      @user = Factory.build(:user)
+      User.stub!(:find, @user.id).and_return(@user)
+    end
+    
     it "should be successful" do
       get 'new'
       response.should be_success
@@ -12,6 +18,11 @@ describe UsersController do
     it "should have the right title" do
       get 'new'
       response.should have_tag("title", /Sign Up/)
+    end
+    
+    it "should have a profile image" do
+      get :show, :id => @user
+      response.should have_tag('h2>img', :class => 'gravatar')
     end
     
   end
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                   "Michael Jackson"
+  user.email                  'mj@sonymusic.com'
+  user.password               'moon walker'
+  user.password_confirmation  'moon walker'
+end
