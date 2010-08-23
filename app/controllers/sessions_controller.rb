e8c7eb61486c01b3fe235fbe320a7a577f63278c
Wupher Fan <wupher.fan@gmail.com>
2010-08-23 09:43:49 +0800

    sign in ok

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,22 @@
 class SessionsController < ApplicationController
   def new
+    @title = 'Sign In'
   end
 
+  def destory
+  end
+  
+  def create
+    email =  params[:session][:email]
+    password = params[:session][:password]
+    user = User.authenticate(email, password)
+    if user.nil?
+      flash[:error] = "Illegal user email or password"
+      @title = 'Sign In'
+      render 'new'
+    else
+      sign_in user
+      redirect_to user
+    end
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,26 @@
 module SessionsHelper
+  def sign_in(user)
+    user.remember_me!
+    cookies[:remember_token] = {:value => user.remember_token, :expire => 20.years.from_now.utc }
+    self.current_user = user
+  end
+  
+  def current_user=(user)
+    @current_user = user
+  end
+  
+  def current_user
+    #We just can't simply return @current_user because we want this func can be used in another pages and controllers
+    # without @current_user
+    @current_user ||= user_from_remember_token
+  end
+  
+  def user_from_remember_token
+    remember_token = cookies[:remember_token]
+    User.find_by_remember_token(remember_token) unless remember_token.nil?
+  end
+  
+  def signed_in?
+    !current_user.nil?
+  end
 end
